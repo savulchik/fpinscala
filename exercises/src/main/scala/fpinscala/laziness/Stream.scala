7ed0a5b2a5c5a64c254df9b4da63344116fcf70c
Stanislav Savulchik <s.savulchik@gmail.com>
2015-08-09 16:22:34 +0600

    Stream#forAll

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -39,7 +39,7 @@ trait Stream[+A] {
       else empty
   }
 
-  def forAll(p: A => Boolean): Boolean = sys.error("todo")
+  def forAll(p: A => Boolean): Boolean = foldRight(true) { (a, b) => p(a) && b }
 
   def headOption: Option[A] = sys.error("todo")
 
